5205ef34ba6c6939f5e9f8da5e8fa135fdb118eb
F Bliss <60014136+voffee@users.noreply.github.com>
2023-04-24 07:57:33 +0800

    Added JS Functionality (test)

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
 
-document.querySelector('.cardholder-name-input');
+// Define UI Variables
+const cardholderNameInput = document.querySelector('.cardholder-name-input');
+const cardholderNameDisplay = document.querySelector('.cardholder-name-display p:nth-child(2)');
+
+// Add event listeners
+cardholderNameInput.addEventListener('input', () => {
+    cardholderNameDisplay.textContent = cardholderNameInput.value;
+});
